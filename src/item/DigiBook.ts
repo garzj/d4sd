24540c0f319b7a28ef7d984c56eddedd2a54d1c8
garzj <jojogarz123@gmail.com>
2022-04-03 13:39:54 +0200

    Merge pdf pages after download

--- src/item/DigiBook.ts
+++ src/item/DigiBook.ts
@@ -1,9 +1,12 @@
 import { promisePool } from '@/util/promise';
+import { createWriteStream } from 'fs';
+import { readFile, rm } from 'fs/promises';
 import { join } from 'path';
+import * as pdf from 'pdfjs';
 import { Item } from './Item';
 
 export class DigiBook extends Item {
-  async download(outDir: string, concurrency = 10) {
+  async download(outDir: string, concurrency = 10, mergePdfs = true) {
     const dir = await this.mkSubDir(outDir);
 
     // Get url of 1st svg page
@@ -22,6 +25,11 @@ export class DigiBook extends Item {
     }
 
     // Page download pool
+    let pages = 0;
+    function getPdfPath(pageNo: number) {
+      return join(dir, `${pageNo}.pdf`);
+    }
+
     await promisePool(async (i, stop) => {
       const pageNo = i + 1;
 
@@ -40,13 +48,34 @@ export class DigiBook extends Item {
         if (!res.ok()) return stop();
 
         // Save it as pdf
+        const pdfFile = getPdfPath(++pages);
+
         await page.pdf({
           format: 'a4',
-          path: join(dir, `${pageNo}.pdf`),
+          path: pdfFile,
         });
       } finally {
         await page.close();
       }
     }, concurrency);
+
+    // Merge pdf pages
+    if (mergePdfs) {
+      const mergedDoc = new pdf.Document();
+
+      for (let pageNo = 1; pageNo <= pages; pageNo++) {
+        const pdfFile = getPdfPath(pageNo);
+        const buffer = await readFile(pdfFile);
+        const pdfDoc = new pdf.ExternalDocument(buffer);
+        mergedDoc.addPagesOf(pdfDoc);
+      }
+
+      const writeStream = createWriteStream(`${dir}.pdf`);
+      mergedDoc.pipe(writeStream);
+      await mergedDoc.end();
+      await new Promise((resolve) => writeStream.once('close', resolve));
+
+      await rm(dir, { recursive: true });
+    }
   }
 }
