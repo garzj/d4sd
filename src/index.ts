54bbe05f053d2eb785ad928e817af4819b2a2a0c
garzj <jojogarz123@gmail.com>
2022-04-03 15:50:01 +0200

    Fix missing exports

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,23 @@
 import './config/env';
 
+import { Shelf } from './Shelf';
 import { Archive } from './item/Archive';
 import { DigiBook } from './item/DigiBook';
+import { DigiDoc } from './item/DigiDoc';
+import { Folder } from './item/Folder';
 import { Item } from './item/Item';
+import { ItemGroup } from './item/ItemGroup';
 import { ItemRef } from './item/ItemRef';
 import { ScookBook } from './item/ScookBook';
-import { Shelf } from './Shelf';
 
-export { Shelf, ItemRef, Item, Archive, DigiBook, ScookBook };
+export {
+  Shelf,
+  Archive,
+  DigiBook,
+  DigiDoc,
+  Folder,
+  Item,
+  ItemGroup,
+  ItemRef,
+  ScookBook,
+};
