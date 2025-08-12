9971d0acc42299252eaca10dd9534146778a4297
garzj <johannes@garz.dev>
2025-08-12 22:54:47 +0200

    fix: bibox downloads

--- src/item/BiBoxBook.ts
+++ src/item/BiBoxBook.ts
@@ -1,4 +1,5 @@
-import { promisePool } from '../util/promise';
+import { basename } from 'path';
+import { delay, promisePool } from '../util/promise';
 import { Book } from './Book';
 import { defDownloadOptions, DownloadOptions } from './download-options';
 import { getPdfOptions } from './get-pdf-options';
@@ -8,45 +9,81 @@ export class BiBoxBook extends Book {
     const dir = await this.mkSubDir(outDir);
     const options = defDownloadOptions(_options);
 
-    // Get page urls
-    let pageUrls: string[] = [];
+    // get first page url
+    let pageCount: number;
+    const pageUrls = new Map<number, URL>();
 
     const page = await this.shelf.browser.newPage();
     try {
+      page.on('request', (req) => {
+        const url = new URL(req.url());
+        if (!url.pathname.startsWith('/bookpages')) return;
+        const base = basename(url.pathname);
+        const pageNoMatch = base.match(/[0-9]+/);
+        if (!pageNoMatch) return;
+        const pageNo = parseInt(pageNoMatch[0]);
+        pageUrls.set(pageNo, url);
+      });
+
+      const bookFrameSelector = '#book-frame';
+
       await page.goto(this.url, {
         waitUntil: 'load',
         timeout: this.shelf.options.timeout,
       });
+      await page.waitForSelector(bookFrameSelector);
 
-      const flipSelector = 'button#flip-right';
-      await page.waitForSelector(flipSelector);
+      // Nav to first page
+      const pageUrl = new URL(page.url());
+      pageUrl.pathname = pageUrl.pathname.replace(
+        /(?<=book\/[0-9]*)(\/|$).*/,
+        '/page/1'
+      );
+      await page.goto(pageUrl.toString(), {
+        timeout: this.shelf.options.timeout,
+      });
 
-      // show only a single page
-      await (
-        await page.$(
-          'button#page-mode:has(mat-icon[data-mat-icon-name="einzelseite"])'
+      // Only show single pages
+      const singlePageSelector = '#page-nav-layout .page-nav-button';
+      const singlePageBtn = await page.waitForSelector(singlePageSelector);
+
+      // Close popup
+      const popupBtn = await page.$('.popup button');
+      if (popupBtn) {
+        await popupBtn.click();
+      }
+
+      await singlePageBtn?.click();
+      await page.waitForSelector('.toggle-button.page-nav-button.selected');
+
+      // Find page count, trigger page requests
+      const flipSelector = '.page-nav-button.right';
+      const flipBtn = (await page.waitForSelector(flipSelector))!;
+
+      pageCount = 1;
+      while (
+        await page.evaluate(
+          (s) => document.querySelector(s)?.ariaDisabled !== 'true',
+          flipSelector
         )
-      )?.click();
-      await page.waitForSelector('mat-icon[data-mat-icon-name="doppelseite"]');
+      ) {
+        await flipBtn.click();
+        pageCount++;
+      }
 
-      while (true) {
-        await page.waitForSelector('app-page img');
-        const urls = await page.$$eval('app-page img', (pageElms) =>
-          pageElms.map((elm) => elm.src)
-        );
-        pageUrls.push(...urls);
-
-        if (await page.$eval(flipSelector, (btn) => btn.disabled)) {
-          break;
+      // Wait for page requests
+      const startTime = Date.now();
+      while (pageUrls.size !== pageCount) {
+        if (Date.now() - startTime > this.shelf.options.timeout) {
+          throw 'Timeout when waiting for page urls.';
         }
-        await page.click(flipSelector);
+        await delay(100);
       }
     } finally {
       await page.close();
     }
 
     // Page download pool
-    const pageCount = pageUrls.length;
     let downloadedPages = 0;
     const getProgress = () => ({
       item: this,
@@ -59,11 +96,14 @@ export class BiBoxBook extends Book {
     await promisePool(
       async (i) => {
         const pageNo = i + 1;
-        const pageUrl = pageUrls[i];
+        const pageUrl = pageUrls.get(pageNo);
+        if (!pageUrl) {
+          throw `Page ${pageNo} has not been requested.`;
+        }
 
         const page = await this.shelf.browser.newPage();
         try {
-          await page.goto(pageUrl, {
+          await page.goto(pageUrl.toString(), {
             waitUntil: 'domcontentloaded',
             timeout: this.shelf.options.timeout,
           });
@@ -76,6 +116,7 @@ export class BiBoxBook extends Book {
             path: pdfFile,
           });
 
+          downloadedPages++;
           options.onProgress(getProgress());
         } finally {
           await page.close();
